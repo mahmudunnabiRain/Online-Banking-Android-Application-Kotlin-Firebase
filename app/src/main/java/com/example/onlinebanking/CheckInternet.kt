6dede33d50b509dc62a6910d794591798a7888fb
mahmudunnabiRain <52375732+mahmudunnabiRain@users.noreply.github.com>
2019-07-13 21:39:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/onlinebanking/CheckInternet.kt
@@ -0,0 +1,28 @@
+package com.example.onlinebanking
+
+import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import io.opencensus.stats.View
+
+class CheckInternet(private val context: Context) {
+
+
+    fun checkNow():Boolean
+    {
+        val cm  = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = cm.activeNetworkInfo
+
+        if(networkInfo != null && networkInfo.isConnected)
+        {
+            return true
+        }
+        else
+            Toast.makeText(context,"No internet Connection", Toast.LENGTH_SHORT).show()
+            return false
+    }
+
+}
---
+++ app/src/main/java/com/example/onlinebanking/MyFiretore.kt
@@ -0,0 +1,13 @@
+package com.example.onlinebanking
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MyFiretore {
+
+    fun lol(amount: String) : Boolean
+    {
+
+        return true
+    }
+
+}
